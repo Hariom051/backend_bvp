77a18d9c6c872fa7abaf970eb3505a888829ceb1
Hariom Arya <114311946+Hariom051@users.noreply.github.com>
2023-08-23 12:04:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+// Entry File.
+//const express = require('express'); // Common JS
+import express from 'express';
+import {userRoutes} from './routes/user-routes.js';
+import {ideRoutes} from './routes/ide-routes.js';
+import cors from 'cors';
+import dotenv from "dotenv"
+const app = express();
+app.use(cors());
+dotenv.config();
+// cors middleware
+app.use(express.json()); // JSON Data Fetch (next)
+app.use('/',userRoutes);
+//app.use('/', ideRoutes);
+// Last Middleware (404)
+console.log("kj",process.env.USERNAME)
+app.use((request, response, next)=>{
+    response.json({message:'Invalid URL'});
+})
+const server = app.listen(1234,(err)=>{
+    if(err){
+        console.log('Server Crash ', err);
+    }
+    else{
+        console.log('Server Up and Running ', server.address().port);
+    }
+})
